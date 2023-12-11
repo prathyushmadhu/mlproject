3973196b5c82327bdee17294351f7a468bb7dcc5
prathyushmadhu <prathyushmadhu@gmail.com>
2023-12-11 13:38:41 +0530

    Model trainer done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,17 +17,17 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object, evaluate_models
+from src.utils import save_object, evaluate_model
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path("artifacts", "model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("split training and test input data")
             X_train, y_train, X_test, y_test = (
@@ -48,8 +48,29 @@ class ModelTrainer:
                 "AdaBoost Classifier": AdaBoostRegressor(),
             }
         
-            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test = y_test, models=models)
+            model_report:dict=evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test = y_test, models=models)
+
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path = self.model_trainer_config.trained_model_file_path,
+                obj = best_model
+            )
+
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
         
 
-        except:
-            pass
+        except Exception as e:
+            raise CustomException(e, sys)
